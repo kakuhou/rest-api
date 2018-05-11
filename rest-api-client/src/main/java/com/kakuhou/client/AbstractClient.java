2c723880417e7f215de019cf4d7fa75eea099818
guopeng <guopeng@172.28.103.73>
2018-05-11 17:19:34 +0800

    初始化

--- rest-api-client/src/main/java/com/kakuhou/client/AbstractClient.java
+++ rest-api-client/src/main/java/com/kakuhou/client/AbstractClient.java
@@ -7,6 +7,7 @@ import java.security.SignatureException;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import com.kakuhou.base.Msg;
 import com.kakuhou.base.Rq;
@@ -35,7 +36,7 @@ public abstract class AbstractClient {
 
 	public abstract Client initClient(String domain, PublicKey publicKey, PrivateKey privateKey, String clientId);
 
-	public <T> Rt<T> doRequest(Rq rq, Type type) throws Exception {
+	public <T> Rt<T> doRequest(Rq rq) throws Exception {
 		String body = gson.toJson(rq);
 		Msg msg = new Msg();
 		msg.setData(Base64Utils.encode(RsaUtil.encryptByPublicKey(body.getBytes(), publicKey)));
@@ -43,7 +44,7 @@ public abstract class AbstractClient {
 		Url url = rq.getClass().getAnnotation(Url.class);
 		Msg reMsg = doHttp(domain + url.value(), msg, clientId);
 		verify(reMsg);
-		return Msg2Rt(reMsg, type);
+		return Msg2Rt(reMsg);
 	}
 
 	private Msg doHttp(String url, Msg msg, String clientId) throws Exception {
@@ -61,8 +62,10 @@ public abstract class AbstractClient {
 		}
 	}
 
-	private <T> Rt<T> Msg2Rt(Msg msg, Type type) throws Exception {
+	@SuppressWarnings("serial")
+	private <T> Rt<T> Msg2Rt(Msg msg) throws Exception {
 		String json = new String(RsaUtil.decryptByPrivateKey(Base64Utils.decode(msg.getData()), privateKey));
-		return gson.fromJson(json, type);
+		Type type2 = new TypeToken<Rt<T>>() {}.getType();
+		return gson.fromJson(json, type2);
 	}
 }
